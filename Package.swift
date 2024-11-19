d1e77bed6b3135a89b1da23e572194c68b7571a0
Gagan Vishal Mishra <gagan_vishal.mishra@mail.schwarz>
2024-11-19 15:35:50 +0100

    first commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "shared",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "shared",
+            targets: ["shared"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "shared",
+            path: "./shared.xcframework"
+        )
+    ]
+)
+
